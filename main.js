51adfb00e2f540529a52497c36d0050f684dd9bd
dherrera.98 <dherrera.98@outlook.com>
2018-05-13 15:29:50 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,118 @@
+"use strict"
+//Componente footer
+Vue.component("footer-comp",{
+    props:["tareas"],
+    template: `
+    <footer>
+        <p>Totales: <span>{{total}}</span></p>
+        <p>Sin completar: <span>{{sinCompletar}}</span></p>
+        <p>Completadas: <span>{{completada}}</span></p>
+    </footer>`,
+    computed:{
+        total(){
+            if(this.tareas != null){
+                return this.tareas.length
+            }
+        },
+        sinCompletar(){
+            var contador = 0
+            this.tareas.forEach(tarea => {
+                if(tarea.completada == false){
+                     contador++
+                }
+            });
+            return contador
+        },
+        completada(){
+             var contador = 0
+             this.tareas.forEach(tarea => {
+                 if(tarea.completada){
+                      contador++
+                 }
+             });
+             return contador
+        }
+    }
+})
+
+//Componente header
+Vue.component("header-comp",{
+    props:["titulo"],
+    template:"#header-temp"
+})
+
+//Componente header
+Vue.component("anadirtarea-comp",{
+    props:["tareas"],
+    template:`
+    <div class="anadirTarea">
+        <input type="text" v-model="inputNuevo" @keyup.enter="anadir()" placeholder="Nueva tarea" :maxlength="max">
+    </div>`,
+    data(){
+        return {
+            inputNuevo: ""
+        }
+    },
+    methods:{
+        anadir(){
+            if(this.inputNuevo != ""){
+                this.tareas.unshift({
+                    nombre: this.inputNuevo,
+                    prioridad: false,
+                    completada: false
+                })
+                this.inputNuevo = ""
+            }
+            let tareasStorage = localStorage.setItem("tareas",JSON.stringify(this.tareas))
+        }
+    },
+    computed:{
+        max(){
+            return screen.width/17
+           }
+    }
+    })
+
+//componente tareas
+Vue.component("tareas-comp",{
+    props:["tareas"],
+    template:"#tareas-temp",
+    methods:{
+        eliminarTarea(key){
+            this.tareas.splice(key,1)
+            let tareasStorage = localStorage.setItem("tareas",JSON.stringify(this.tareas))
+        },
+
+        completarTarea(key){
+            this.tareas[key].completada = !this.tareas[key].completada
+            let tareasStorage = localStorage.setItem("tareas",JSON.stringify(this.tareas))
+        },
+    },
+    filters:{
+        upperCase(tarea){
+            tarea.toString()
+            return tarea[0].toUpperCase() + tarea.slice(1,tarea.length).toLowerCase()
+        },
+        upperCase(data){
+            data.toString()
+            return data[0].toUpperCase() + data.slice(1,data.length).toLowerCase()
+            return data[0].toUpperCase() + data.slice(1,data.length)
+        },
+    }
+})
+
+//instancia
+const app = new Vue({
+    el:"#app",
+    data:{
+        titulo:"TodoList",
+        tareas:[],
+        inputNuevo: "",
+    },
+    beforeMount(){
+        if(JSON.parse(localStorage.getItem("tareas") != null)){
+            this.tareas = JSON.parse(localStorage.getItem("tareas"))
+        }
+    },
+
+})
